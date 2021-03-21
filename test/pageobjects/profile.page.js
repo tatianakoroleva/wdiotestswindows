4561f08905da36a3b86d51e077ba775a8fac2d6a
TatianaKoroleva <korolevatatiana77@gmail.com>
2021-03-20 22:58:02 -0700

    first test

---
+++ test/pageobjects/profile.page.js
@@ -0,0 +1,14 @@
+import Page from './page';
+import * as url from "url";
+
+class ProfilePage extends Page{
+
+    get iconUser(){ return $('.ant-avatar-square');
+    }
+    isOpen(){
+      expect(browser).toHaveUrlContaining('/profile/');
+      expect(this.iconUser).toBeDisplayed();
+  }
+}
+
+export default new ProfilePage()
