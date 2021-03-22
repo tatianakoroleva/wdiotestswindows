0c6d9a6e442c6c09f9b6baf41365402d74eef5e5
TatianaKoroleva <korolevatatiana77@gmail.com>
2021-03-22 00:13:28 -0700

    first test adds

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -6,6 +6,11 @@ class LoginPage extends Page {
     get inputUsername () { return $('#normal_login_email'); }
     get inputPassword () { return $('#normal_login_password') ;}
     get buttonSubmit () { return $('.login-form-button'); }
+    get errorToast () { return $('.ant-notification-notice-message'); }
+
+    open () {
+        return super.open('/');
+    }
 
     setLogin (email){
         this.inputUsername.setValue(email);
@@ -18,10 +23,13 @@ class LoginPage extends Page {
     clickSubmitButton (){
         this.buttonSubmit.click();
     }
-
-    open () {
-        return super.open('/');
+    submitButtonIsDisabled(){
+        expect(this.buttonSubmit).toBeDisabled();
     }
+    errorToastAppeared() {
+        expect(this.errorToast).toBeDisplayed();
+    }
+
 }
 
 export default new LoginPage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,13 +2,29 @@ import LoginPage from  '../pageobjects/login.page';
 import ProfilePage from '../pageobjects/profile.page';
 
 describe('Auth', () => {
-    it('user logs in with valid data', () => {
+    beforeEach( () => {
         LoginPage.open();
+    });
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()');
+    });
+
+    it.skip('user logs in with valid data', () => {
         LoginPage.setLogin('fejoya8091@heroulo.com');
         LoginPage.setPassword('t1875456');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
         });
+    it.skip('submit button is disabled if login and password are absent', () => {
+        LoginPage.submitButtonIsDisabled();
+    });
+    it('fails if invalid data provided', () => {
+       LoginPage.setLogin('example@examplr.com');
+       LoginPage.setPassword('5656565656');
+       LoginPage.clickSubmitButton();
+       LoginPage.errorToastAppeared();
+    });
+
 });
 
 
